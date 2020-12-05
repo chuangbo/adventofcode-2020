355c7008c4d68c441bff58e848d8642ee069f816
Li Chuangbo <im@chuangbo.li>
2020-12-05 19:35:44 +0800

    Day 1

---
+++ day1/d1.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+import math
+
+filename = 'example.txt'
+filename = 'input.txt'
+
+lst = [int(line.strip()) for line in open(filename)]
+
+def find_sum(n):
+    for c in combinations(lst, n):
+        if sum(c) == 2020:
+            return math.prod(c)
+
+print('part1:', find_sum(2))
+print('part2:', find_sum(3))
